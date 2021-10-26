7d6d4a619515bf09412245e149eb0f37392e8e1c
Karl Hobley <karl@torchbox.com>
2021-10-26 13:52:51 +0100

    No longer necessary to add 'wagtail.admin' to INSTALLED_APPS

--- wagtail/wagtail_hooks/__init__.py
+++ wagtail/wagtail_hooks/__init__.py
@@ -3,5 +3,5 @@ Temporarily made this into a folder to make it easier to automate the merging
 Will concatenate these files later
 """
 
-from .core import *  # noqa
 from .admin import *  # noqa
+from .core import *  # noqa
